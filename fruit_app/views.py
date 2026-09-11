2b50b4984ebae25efe57b5428b0406abc0e634bf
Zohreh Habibi <habibi.z@outlook.de>
2026-09-11 10:52:28 +0200

    feat: add send fruits view

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def send_fruits(request):
+
+    # fruits - List containing 5 fruits as dictionaries
+    fruits = [
+        {
+            'name': 'Apple',
+            'color': 'Red',
+            'weight': 150
+        },
+        {
+            'name': 'Banana',
+            'color': 'Yellow',
+            'weight': 120
+        },
+        {
+            'name': 'Mango',
+            'color': 'Yellow',
+            'weight': 200
+        },
+        {
+            'name': 'Grapes',
+            'color': 'Purple',
+            'weight': 50
+        },
+        {
+            'name': 'Lemon',
+            'color': 'Yellow',
+            'weight': 100
+        }
+    ]
+
+    return HttpResponse(
+        json.dumps({'fruits': fruits}),
+        content_type='application/json'
+    )

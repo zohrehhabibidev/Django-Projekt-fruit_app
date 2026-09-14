2f9ffeaf1100cab9f2888894ec22172151e998ed
Zohreh Habibi <habibi.z@outlook.de>
2026-09-14 14:04:09 +0200

    feat: add Fruit model and initial database migration

--- fruit_app/models.py
+++ fruit_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
+    weight = models.IntegerField()

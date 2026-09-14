be43914da1ea383395208cab0473cf7e414bf6e1
Zohreh Habibi <habibi.z@outlook.de>
2026-09-14 17:28:11 +0200

    feat: extend Fruit model with optional fields

--- fruit_app/models.py
+++ fruit_app/models.py
@@ -7,3 +7,7 @@ class Fruit(models.Model):
     name = models.CharField(max_length=30)
     color = models.CharField(max_length=30)
     weight = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    is_available = models.BooleanField(default=True)
+    description = models.TextField(blank=True)
+    origin_country = models.CharField(max_length=50, null=True)
